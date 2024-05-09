4f3f86d742cc463565c0cb74adab9d13bc957550
hiagoroedel <hiagojunio4@gmail.com>
2024-05-09 12:00:16 -0300

    strapi

---
+++ src/api/polo/controllers/polo.ts
@@ -0,0 +1,7 @@
+/**
+ * polo controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::polo.polo');
---
+++ src/api/polo/routes/polo.ts
@@ -0,0 +1,7 @@
+/**
+ * polo router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::polo.polo');
